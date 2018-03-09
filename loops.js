9b54303bd79c7e3aed748c708c310ec03206c739
Lisa Connell <connell.lisamarie@gmail.com>
2018-03-09 19:04:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,20 @@
 function forLoop(array) {
-  for ()
+  for (let i = 0; i < 25; i++) {
+  if (i ===1) {
+    array.push("I am 1 strange loop.")
+  } else {
+    array.push("I am ${i} strange loops.")
+  }  
+  }
+  return array
+}
+
+function whileloop(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+  return 'done'
+  }
+function doWhileLoop(array) {
+  
 }
